9b0ea7862e6c62d7aa16ad2b1a694de9aaa67e15
Yogesh <yogeshmane9929@gmail.com>
2019-03-20 16:26:00 +0530

    react-ui-done

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-
+import ShoppingList from './components/ShoppingList';
+import { Provider } from 'react-redux';
+import store from './store';
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
       <AppNavbar/>
+      <ShoppingList/>
       </div>
+      </Provider>
     );
   }
 }
